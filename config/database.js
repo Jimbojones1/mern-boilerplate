b11738d20cb679e9da563a879af240d9bff7b233
Jim Haff <Jimmyhaff3@gmail.com>
2020-10-07 13:48:06 -0500

    added db config

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  'mongodb://localhost:27017/puppies',
+  { useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  }
+);
+
+const db = mongoose.connection;
+
+db.on('connected', function() {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+});
