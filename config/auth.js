1a2ed6842531f3c5a1ec475f079bee432fa7cd89
Jim Haff <Jimmyhaff3@gmail.com>
2022-09-20 23:50:21 -0500

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,21 +3,24 @@ const SECRET = process.env.SECRET;
 
 module.exports = function(req, res, next) {
   // Check for the token being sent in three different ways
-  const token = req.get('Authorization') || req.query.token || req.body.token;
+  let token = req.get('Authorization') || req.query.token || req.body.token;
   if (token) {
     // Remove the 'Bearer ' if it was included in the token header
     token = token.replace('Bearer ', '');
     // Check if token is valid and not expired
     jwt.verify(token, SECRET, function(err, decoded) {
       if (err) {
-        next(err);
+        req.user = null;
+        return next();
       } else {
-        // It's a valid token, so add user to req
-        req.user = decoded.user;    
-        next();
+         // If invalid token, err will be set
+    
+        return next(); // passes the req object to the next place in the middleware chain!
       }
     });
   } else {
+    // not token
+    req.user = null;
     next();
   }
 };
